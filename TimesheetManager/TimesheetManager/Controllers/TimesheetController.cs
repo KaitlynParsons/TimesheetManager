9c4d7a9520fe14fdeecba16ee0a6a3f696c7c604
Kaitlyn Parsons <38901138+KaitlynParsons@users.noreply.github.com>
2019-02-02 22:40:04 +1000

    Add files via upload

---
+++ TimesheetManager/TimesheetManager/Controllers/TimesheetController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TimesheetManager.Models;
+
+namespace TimesheetManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimesheetController : ControllerBase
+    {
+        private readonly TimesheetContext _context;
+
+        public TimesheetController(TimesheetContext context)
+        {
+            _context = context;
+        }
+
+        //list all 
+        // GET: api/Timesheet
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TimesheetModel>>> GetTimesheets()
+        {
+            return await _context.TimesheetsModel.Include(time => time.timesheetTotals).ToListAsync();
+        }
+
+        //list one
+        // GET: api/Timesheet/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TimesheetModel>> GetTimesheet(long id)
+        {
+            var Timesheet =  await _context.TimesheetsModel.Include(time => time.timesheetTotals)
+            .SingleAsync(timesheet => timesheet.Id == id);
+
+            if (Timesheet == null)
+            {
+                return NotFound();
+            }
+
+            return Timesheet;
+        }
+
+        //create
+        // POST: api/Timesheet
+        [HttpPost]
+        public async Task<ActionResult<TimesheetModel>> PostTimesheet(TimesheetModel timesheet)
+        {
+            _context.TimesheetsModel.Add(timesheet);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTimesheets), new { id = timesheet.Id }, timesheet);
+        }
+
+        //update
+        // PUT: api/Timesheet/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTimesheet(long id, TimesheetModel timesheet)
+        {
+            if (id != timesheet.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(timesheet).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE: api/Timesheet/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var timesheet = await _context.TimesheetsModel.FindAsync(id);
+
+            if (timesheet == null)
+            {
+                return NotFound();
+            }
+
+            _context.TimesheetsModel.Remove(timesheet);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ TimesheetManager/TimesheetManager/Models/TimesheetModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace TimesheetManager.Models
+{
+    public enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    public class TimesModel
+    {
+        public long Id { get; set; }
+        public Days Day { get; set; }
+        public string Start { get; set; }
+        public string LunchStart { get; set; }
+        public string LunchEnd { get; set; }
+        public string End { get; set; }
+    }
+
+    public class TimesheetModel
+    {
+        public long Id { get; set; }
+        public string employee { get; set; }
+        public string startPeriod { get; set; }
+        public string endPeriod { get; set; }
+        public string payDate { get; set; }
+        public List<TimesModel> timesheetTotals { get; set; }
+        public bool paidBreaks { get; set; }
+        public string signature { get; set; }
+        public string date { get; set; }
+    }
+}
